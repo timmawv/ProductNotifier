3c4d4a1e29d3bc5849181f4c696b6b9eb162336e
Timur <avlyakulovtimur@gmail.com>
2024-09-26 10:05:27 +0300

    created shared-lib for dto

---
+++ shared-lib/events-dto/src/main/java/avlyakulov/timur/event/ProductCreatedEvent.java
@@ -0,0 +1,23 @@
+package avlyakulov.timur.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductCreatedEvent {
+
+    private String productId;
+
+    private String title;
+
+    private BigDecimal price;
+
+    private Integer quantity;
+}
